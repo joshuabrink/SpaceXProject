eac4cc94e41b109a875ddce124cf427cff76601c
Leonardo-lastingIT <63236683+Leonardo-lastingIT@users.noreply.github.com>
2021-11-15 16:05:29 +0200

    Setup Basics

---
+++ cmake-build-debug/CMakeFiles/SpaceXProject.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/Falcon9Stage1.cpp" "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/cmake-build-debug/CMakeFiles/SpaceXProject.dir/Falcon9Stage1.cpp.obj"
+  "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/Falcon9Stage2.cpp" "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/cmake-build-debug/CMakeFiles/SpaceXProject.dir/Falcon9Stage2.cpp.obj"
+  "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/FalconHeavyStage1.cpp" "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/cmake-build-debug/CMakeFiles/SpaceXProject.dir/FalconHeavyStage1.cpp.obj"
+  "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/FalconHeavyStage2.cpp" "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/cmake-build-debug/CMakeFiles/SpaceXProject.dir/FalconHeavyStage2.cpp.obj"
+  "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/StageBuilder.cpp" "C:/Users/Leonardo/Documents/2021/COS214/Assignments/Group Project/SpaceXProject/cmake-build-debug/CMakeFiles/SpaceXProject.dir/StageBuilder.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SpaceXProject.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceXProject.dir/Falcon9Stage1.cpp.obj"
+  "CMakeFiles/SpaceXProject.dir/Falcon9Stage2.cpp.obj"
+  "CMakeFiles/SpaceXProject.dir/FalconHeavyStage1.cpp.obj"
+  "CMakeFiles/SpaceXProject.dir/FalconHeavyStage2.cpp.obj"
+  "CMakeFiles/SpaceXProject.dir/StageBuilder.cpp.obj"
+  "SpaceXProject.exe"
+  "SpaceXProject.exe.manifest"
+  "SpaceXProject.pdb"
+  "libSpaceXProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceXProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
