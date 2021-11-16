3dac8e464f9d43e59677a13132d5a9b0cb101ac3
SeePeeYou <u17110310@tuks.co.za>
2021-11-16 17:12:20 +0200

    main

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include "Satellite.h"
+#include "SatelliteIterator.h"
+#include "StarlinkCommunication.h"
+#include "StarlinkGroundUsers.h"
+#include "StarlinkOrbitingSatellite.h"
+#include "ConcreteSatelliteIterator.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
