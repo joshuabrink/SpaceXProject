74de6c67f44be062f9e3eb889c741cd8a050dd5d
SeePeeYou <u17110310@tuks.co.za>
2021-11-17 01:31:09 +0200

    doxygen comments for concrete satellite iterator

--- ConcreteSatelliteIterator.cpp
+++ ConcreteSatelliteIterator.cpp
@@ -2,10 +2,25 @@
 
 ConcreteSatelliteIterator::ConcreteSatelliteIterator(/* args */)
 {
+/**
+ * @author      Tshegofatso Manthata
+ * @brief       This is the Concrete Satellite Iterator constructor
+ * @todo        initialize the attributes "first,next,curr and last" to null
+ */
      first=next=curr=last=nullptr;
 }
+/**
+ * @author      Tshegofatso Manthata
+ * @class       ConcreteSatelliteIterator
+ * 
+ * @fn          Satellite* ConcreteSatelliteIterator::firstSat()
+ * @brief       This is the firstSat() function
+ * @todo        return the first satellite in the linked list
+ * @return      A Satellite pointer
+ */
 Satellite* ConcreteSatelliteIterator::firstSat()
 {
+
     return first;
 }
 Satellite* ConcreteSatelliteIterator::lastSat()
