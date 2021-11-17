d67dcb5e162ec1e397f629d8d3cf1babd42baadd
joshuabrink1 <joshuabrink1@gmail.com>
2021-11-17 10:46:05 +0200

    Potential mediator

--- CommuncationNetwork.h
+++ CommuncationNetwork.h
@@ -1,13 +1,27 @@
 #ifndef COMMUNCATIONNETWORK_H
 #define COMMUNCATIONNETWORK_H
-//Mediator
+#include "ComNodeQueue.h"
+
+// Mediator
 class CommuncationNetwork
 {
 private:
-public:
-    CommuncationNetwork();
-    ~CommuncationNetwork();
-};
+    ComNodeQueue col;
 
+public:
+    CommuncationNetwork(){};
+    void notify()
+    {
+        // ComNodeQueueIterator it = col.begin();
+        // while (!(it == col.end()))
+        // {
+        //     // CommuncationNode cur = *(*it);
+        //     // cout << (*it).id << endl;
+            
+        //     ++(it);
+        // }
+    };
+    ~CommuncationNetwork(){};
+};
 
 #endif
