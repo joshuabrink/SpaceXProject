3dd3fcdcece27e785a9ec42f3c4330b39b872a5e
Anru <anrunel.an@gmail.com>
2021-11-20 18:20:36 +0200

    Update Falcon9.h
    Added doxygen

--- GroupProject/Falcon9.h
+++ GroupProject/Falcon9.h
@@ -1,7 +1,7 @@
 #ifndef FALCON9_H
 #define FALCON9_H
 
-/** @file ConcreteSpaceCraftFactory.h
+/** @file Falcon9.h
  *  @class Falcon9
  *  @brief Falcon9 is a concrete receiver in the Command design pattern
  *
@@ -17,12 +17,37 @@ using namespace std;
 
 class Falcon9 : public Rocket {
 private:
-	list<Satelllite*> satelllites;
+	list<Satellite*> satellites;
 public:
+/** 
+ *  @fn     Falcon9(CompositeStage*,double)
+ *  @brief  Falcon9 constructor
+ */
 	Falcon9(CompositeStage*,double);
-	void addSatelllites(Satelllite*);
-	//prototype:
+	
+/** 
+ *  @fn     addSatellites()
+ *  @brief  addSatellites() 
+ *
+ *  @param[in] Satellite*, a Satellite pointer
+ *  @return void
+ */
+	void addSatellites(Satellite*);
+	
+/** 
+ *  @fn     Rocket* clone()
+ *  @brief  clone() acts as a prototype in the receiver class
+ *
+ *  @return Rocket*, a pointer to Rocket
+ */
 	Rocket* clone();
+	
+/** 
+ *  @fn     ~Falcon9()
+ *  @brief  Falcon9 destructor
+ */
+	~Falcon9(){};
+	
 };
 
 #endif
