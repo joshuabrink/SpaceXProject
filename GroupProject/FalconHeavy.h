986ca5fc31d1c8b58030d60af1489e451f7bdb58
Anru <anrunel.an@gmail.com>
2021-11-20 18:20:14 +0200

    Update FalconHeavy.h
    Added doxygen

--- GroupProject/FalconHeavy.h
+++ GroupProject/FalconHeavy.h
@@ -1,15 +1,41 @@
 #ifndef FALCONHEAVY_H
 #define FALCONHEAVY_H
 
+/** @file FalconHeavy.h
+ *  @class FalconHeavy
+ *  @brief FalconHeavy is a concrete receiver in the Command design pattern
+ *
+ *  @author Joshua Young
+ *  @bug No known bugs.
+ */
+
 #include "Rocket.h"
 
 using namespace std;
 
 class FalconHeavy: public Rocket {
 public:
+
+/** 
+ *  @fn     FalconHeavy(CompositeStage*,double)
+ *  @brief  FalconHeavy constructor
+ */
 	FalconHeavy(CompositeStage*,double);
-	//prototype:
+	
+/** 
+ *  @fn     Rocket* clone()
+ *  @brief  clone() acts as a prototype in the receiver class
+ *
+ *  @return Rocket*, a pointer to Rocket
+ */
 	Rocket* clone();
+
+/** 
+ *  @fn     ~FalconHeavy()
+ *  @brief  FalconHeavy destructor
+ */
+	~FalconHeavy(){};
+
 };
 
 #endif
